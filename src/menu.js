be118acc71fe9bbede3cfad2eb28874c0a398bb3
GarryLiu <liurin159@gmail.com>
2024-02-14 16:56:46 +0800

    updata index.html js

---
+++ src/menu.js
@@ -0,0 +1,17 @@
+let oMenu = document.querySelector(".menu");
+let oUl = document.querySelector(".logo ul");
+let oLogo = document.querySelector(".logo");
+console.log(getStyle(oUl, "display"));
+oMenu.onclick = function(){
+  if(getStyle(oUl, "display") == "none"){
+    oUl.style.display = "flex";
+    oLogo.style.backgroundColor = "black";
+  }else{
+    oUl.style.display = "none";
+    oLogo.style.backgroundColor = "rgba(0,0,0,0.35)"
+  }
+}
+
+function getStyle(node, cssStyle){
+  return node.currentStyle ? node.currentStyle[cssStyle] : getComputedStyle(node)[cssStyle];
+}
